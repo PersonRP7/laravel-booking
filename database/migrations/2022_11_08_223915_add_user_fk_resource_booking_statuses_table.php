fc2ef06fa23cbd3d3db7fb0a70754fe1384c7faf
me <avos.mail.dev@gmail.com>
2022-11-08 23:51:51 +0100

    set nullable before constrained in resource_booking_statuses

--- database/migrations/2022_11_08_223915_add_user_fk_resource_booking_statuses_table.php
+++ database/migrations/2022_11_08_223915_add_user_fk_resource_booking_statuses_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::table('resource_booking_statuses', function (Blueprint $table) {
             $table->foreignId('user_id')
+            ->nullable()
             ->constrained('users')
-            ->onDelete('cascade')
-            ->nullable();
+            ->onDelete('cascade');
         });
     }
 
