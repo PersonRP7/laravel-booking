8ee4ace35494b7f18f46776d625f46279f32c82c
me <avos.mail.dev@gmail.com>
2022-11-08 23:28:35 +0100

    ResourceBookingStatus -mcr

---
+++ app/Http/Controllers/ResourceBookingStatusController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ResourceBookingStatus;
+use Illuminate\Http\Request;
+
+class ResourceBookingStatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ResourceBookingStatus  $resourceBookingStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ResourceBookingStatus $resourceBookingStatus)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ResourceBookingStatus  $resourceBookingStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ResourceBookingStatus $resourceBookingStatus)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ResourceBookingStatus  $resourceBookingStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ResourceBookingStatus $resourceBookingStatus)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ResourceBookingStatus  $resourceBookingStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ResourceBookingStatus $resourceBookingStatus)
+    {
+        //
+    }
+}
