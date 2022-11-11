cb050e046c239dc2c19e361bc889fc47de525be9
me <avos.mail.dev@gmail.com>
2022-11-11 18:23:56 +0100

    Created pendings table

---
+++ database/migrations/2022_11_09_170048_create_pendings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pendings', function (Blueprint $table) {
+            $table->id();
+            // user foreign key
+            $table->foreignId('user_id')
+            ->nullable()
+            ->constrained('users')
+            ->onDelete('cascade');
+            // /user foreign key
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pendings');
+    }
+};
